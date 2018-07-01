3adaae1184e3a795c279bb4ec5e87ce3bdb62f46
VJER <celeron015@gmail.com>
2018-07-01 16:46:08 +0200

    ManyToMany insertion bug fixed

--- ApotekaJPA/src/manager/KupovinaManager.java
+++ ApotekaJPA/src/manager/KupovinaManager.java
@@ -1,25 +1,34 @@
 package manager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 
 import model.Kupovina;
 import model.KurirskaSluzba;
 import model.Osoba;
+import model.Stavka;
 
 public class KupovinaManager {
 	public static boolean novaKupovina(KurirskaSluzba kurirskaSluzba,Osoba osoba) {
 		EntityManager em = JPAUtil.getEntityManager();
 		try {
+			em.getTransaction().begin();
 			Kupovina kupovina = new Kupovina();
 			kupovina.setKurirskaSluzba(kurirskaSluzba);
-			kupovina.setStavkas(osoba.getStavkas());
-			em.getTransaction().begin();
-			osoba.setStavkas(new ArrayList<>());
-			em.merge(osoba);
+			List<Stavka> stavkas = osoba.getStavkas();
+//			kupovina.setStavkas(stavkas);
 			kupovina.setOsoba(osoba);
 			em.persist(kupovina);
+			em.flush();
+			List<Kupovina> kupovinas = new ArrayList<>();
+			kupovinas.add(kupovina);
+			for(Stavka s:stavkas){
+				Stavka st = em.find(Stavka.class, s.getIdStavka());
+				st.setKupovinas(kupovinas);
+				em.persist(st);
+			}
 			em.getTransaction().commit();
 			em.close();
 			return true;
@@ -28,5 +37,29 @@ public class KupovinaManager {
 			e.printStackTrace();
 			return false;
 		}
+	}
+	
+	public static boolean ocistiKorpu(Osoba o){
+		EntityManager em = JPAUtil.getEntityManager();
+		try {
+			em.getTransaction().begin();
+			Osoba osoba = em.find(Osoba.class, o.getIdOsoba());
+			osoba.setStavkas(new ArrayList<>());
+			em.persist(osoba);
+			em.getTransaction().commit();
+			em.close();
+			return true;
+		}catch(Exception e){
+			em.close();
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public static void main(String[] args){
+		Osoba o = OsobaManager.getOsobaById("1");
+		KurirskaSluzba k = KurirskaSluzbaManager.findKurirskaSluzbaById("1");
+		novaKupovina(k,o);
+		ocistiKorpu(o);
 	}
 }
--- ApotekaWEB/src/sevlets/KorpaServlet.java
+++ ApotekaWEB/src/sevlets/KorpaServlet.java
@@ -24,52 +24,60 @@ import model.Stavka;
 @WebServlet("/KorpaServlet")
 public class KorpaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public KorpaServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public KorpaServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String poruka3="";
-		poruka3=request.getParameter("poruka3");
+		String poruka3 = "";
+		poruka3 = request.getParameter("poruka3");
 		Osoba ulogovaniKorisnik = (Osoba) request.getSession().getAttribute("ulogovaniKorisnik");
-		List<Stavka> listaStavki=StavkaManager.listaStavki(ulogovaniKorisnik);
-		int cena=OsobaManager.cenaKupovine(ulogovaniKorisnik);
+		List<Stavka> listaStavki = StavkaManager.listaStavki(ulogovaniKorisnik);
+		int cena = OsobaManager.cenaKupovine(ulogovaniKorisnik);
 		request.setAttribute("cena", cena);
-		if(listaStavki!=null) {
+		if (listaStavki != null) {
 			request.setAttribute("stavke", listaStavki);
 		}
-		List<KurirskaSluzba> kurirskeSluzbe=KurirskaSluzbaManager.listaKurirskihSluzbi();
+		List<KurirskaSluzba> kurirskeSluzbe = KurirskaSluzbaManager.listaKurirskihSluzbi();
 		request.setAttribute("kurirskeSluzbe", kurirskeSluzbe);
 		RequestDispatcher rd = getServletContext().getRequestDispatcher("/korpa.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String idLek=request.getParameter("odabraniL");
-		if(idLek==null) {
-			String poruka3="";
+		String idLek = request.getParameter("odabraniL");
+		if (idLek == null) {
+			String poruka3 = "";
 			Osoba ulogovaniKorisnik = (Osoba) request.getSession().getAttribute("ulogovaniKorisnik");
 			String kurirskaSluzbaId = request.getParameter("odabranaKS");
-			KurirskaSluzba kurirskaSluzba=KurirskaSluzbaManager.findKurirskaSluzbaById(kurirskaSluzbaId);
-			if(KupovinaManager.novaKupovina(kurirskaSluzba, ulogovaniKorisnik)) {
-				poruka3="Uspesno izvrsena kupovina!";
-			}else {
-				poruka3="Kupovina nije uspesno izvrsena!";
+			KurirskaSluzba kurirskaSluzba = KurirskaSluzbaManager.findKurirskaSluzbaById(kurirskaSluzbaId);
+			if (KupovinaManager.novaKupovina(kurirskaSluzba, ulogovaniKorisnik)) {
+				if (KupovinaManager.ocistiKorpu(ulogovaniKorisnik)) {
+					poruka3 = "Uspesno izvrsena kupovina!";
+				}else {
+					poruka3 = "Kupovina nije uspesno izvrsena!";
+				}
+			} else {
+				poruka3 = "Kupovina nije uspesno izvrsena!";
 			}
-		}else {
+		} else {
 			Osoba osoba = (Osoba) request.getSession().getAttribute("ulogovaniKorisnik");
 			StavkaManager.ukloniStavkuKorisniku(osoba, idLek);
 		}
