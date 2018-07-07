64d0b41878638b453acca68c5798185a9a4d738b
Unknown <obradovic.risbo@gmail.com>
2018-07-07 16:01:48 +0200

    Zavrseno sve, ispravljeni bugovi

--- ApotekaWEB/WebContent/pregledKupovina.jsp
+++ ApotekaWEB/WebContent/pregledKupovina.jsp
@@ -5,7 +5,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<link rel="stylesheet" type="text/css" href="kupovina.css">
+<link rel="stylesheet" type="text/css" href="brisanje.css">
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
--- ApotekaWEB/src/sevlets/KorpaServlet.java
+++ ApotekaWEB/src/sevlets/KorpaServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.ForEach;
+
 import manager.KupovinaManager;
 import manager.KurirskaSluzbaManager;
 import manager.OsobaManager;
@@ -46,8 +48,12 @@ public class KorpaServlet extends HttpServlet {
 		List<Stavka> listaStavki = StavkaManager.listaStavki(ulogovaniKorisnik);
 		int cena = OsobaManager.cenaKupovine(ulogovaniKorisnik);
 		request.setAttribute("cena", cena);
-		if (listaStavki != null) {
+		if (listaStavki != null && !listaStavki.isEmpty()) {
 			request.setAttribute("stavke", listaStavki);
+			System.out.println("nije null");
+		}else {
+			request.setAttribute("stavke", null);
+			System.out.println("null je");
 		}
 		List<KurirskaSluzba> kurirskeSluzbe = KurirskaSluzbaManager.listaKurirskihSluzbi();
 		request.setAttribute("kurirskeSluzbe", kurirskeSluzbe);
@@ -66,17 +72,29 @@ public class KorpaServlet extends HttpServlet {
 		if (idLek == null) {
 			String poruka3 = "";
 			Osoba ulogovaniKorisnik = (Osoba) request.getSession().getAttribute("ulogovaniKorisnik");
-			String kurirskaSluzbaId = request.getParameter("odabranaKS");
-			KurirskaSluzba kurirskaSluzba = KurirskaSluzbaManager.findKurirskaSluzbaById(kurirskaSluzbaId);
-			if (KupovinaManager.novaKupovina(kurirskaSluzba, ulogovaniKorisnik)) {
-				if (KupovinaManager.ocistiKorpu(ulogovaniKorisnik)) {
-					poruka3 = "Uspesno izvrsena kupovina!";
-				}else {
-					System.out.println("Kupovina nije uspesno izvrsena!");
+			List<Stavka> listaStavki = StavkaManager.listaStavki(ulogovaniKorisnik);
+			if (listaStavki != null && !listaStavki.isEmpty()) {
+				System.out.println("nije null post");
+				String kurirskaSluzbaId = request.getParameter("odabranaKS");
+				KurirskaSluzba kurirskaSluzba = KurirskaSluzbaManager.findKurirskaSluzbaById(kurirskaSluzbaId);
+				if (KupovinaManager.novaKupovina(kurirskaSluzba, ulogovaniKorisnik)) {
+					if (KupovinaManager.ocistiKorpu(ulogovaniKorisnik)) {
+						poruka3 = "Uspesno izvrsena kupovina!";
+						request.setAttribute("poruka3", poruka3);
+					}else {
+						poruka3 = "Kupovina nije uspesno izvrsena!";
+						request.setAttribute("poruka3", poruka3);
+					}
+				} else {
+					poruka3 = "Kupovina nije uspesno izvrsena!";
+					request.setAttribute("poruka3", poruka3);
 				}
-			} else {
+				
+			}else {
 				poruka3 = "Kupovina nije uspesno izvrsena!";
+				request.setAttribute("poruka3", poruka3);
 			}
+			
 		} else {
 			Osoba osoba = (Osoba) request.getSession().getAttribute("ulogovaniKorisnik");
 			StavkaManager.ukloniStavkuKorisniku(osoba, idLek);
